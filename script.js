a3dd6588466d8b5044078572c2f1aa889ed1eca8
Zuzana Volfova <zuzka.volfova@gmail.com>
2022-08-09 10:27:39 +0200

    add task

--- script.js
+++ script.js
@@ -116,3 +116,4 @@ function findMin (numbers){
 console.log(findMin(numbers));
 
 /* fizz buzz */
+/*Fizz buzz is a group word game for children to teach them about division.[1] Players take turns to count incrementally, replacing any number divisible by three with the word "fizz", and any number divisible by five with the word "buzz". */
